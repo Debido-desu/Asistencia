723b3d3f855af213f02f5c883e1a95a09c8252df
gameslegends <israeldqz301@gmail.com>
2023-08-06 18:51:51 -0500

    motivo alert verificación

--- views/table/validarex.php
+++ views/table/validarex.php
@@ -14,10 +14,10 @@ if (isset($_POST['valida']) && isset($_POST['id']) && isset($_POST['motivo'])) {
     $id = validar($_POST['id']);
     $motivo = validar($_POST['motivo']);
 
-    if ($motivo !== ''){
+    if ($motivo !== '') {
         $sqlin = "UPDATE estudiantes SET validar='$validar', motivo='$motivo' WHERE id = $id";
         $resultin = mysqli_query($conectar, $sqlin);
-    
+
         if ($resultin) {
             header("Location: inasistencia.php?error= Datos agregados con éxito.");
             exit();
@@ -25,9 +25,9 @@ if (isset($_POST['valida']) && isset($_POST['id']) && isset($_POST['motivo'])) {
             header("Location: inasistencia.php?error= Datos <b>NO</b> agregados..");
             exit();
         }
-    } else{
-        header("Location: inasistencia.php?error= No se han seleccionado un motivo.");
-            exit();
+    } else {
+        echo '<script language="javascript">alert("No se ha seleccionado un motivo.");window.location.href="inasistencia.php?error= No se han seleccionado un motivo."</script>';
+        exit();
     }
 } else {
     header("Location: inasistencia.php");
